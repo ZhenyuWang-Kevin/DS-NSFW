170568ac023693ebf783184b6f098e13b53d53e6
ZhenyuWang-Kevin <zhenyuw6@student.unimelb.edu.au>
2019-04-08 14:04:41 +1000

    Initialization

---
+++ BitBoxSkeleton/src/main/java/unimelb/bitbox/ServerMain.java
@@ -0,0 +1,25 @@
+package unimelb.bitbox;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Logger;
+
+import unimelb.bitbox.util.Configuration;
+import unimelb.bitbox.util.FileSystemManager;
+import unimelb.bitbox.util.FileSystemObserver;
+import unimelb.bitbox.util.FileSystemManager.FileSystemEvent;
+
+public class ServerMain implements FileSystemObserver {
+	private static Logger log = Logger.getLogger(ServerMain.class.getName());
+	protected FileSystemManager fileSystemManager;
+	
+	public ServerMain() throws NumberFormatException, IOException, NoSuchAlgorithmException {
+		fileSystemManager=new FileSystemManager(Configuration.getConfigurationValue("path"),this);
+	}
+
+	@Override
+	public void processFileSystemEvent(FileSystemEvent fileSystemEvent) {
+		// TODO: process events
+	}
+	
+}
