122c252854d344b740e93e3d442368a2790c4d98
ZhenyuWang-Kevin <42224040+ZhenyuWang-Kevin@users.noreply.github.com>
2019-04-29 18:09:01 +1000

    request and response in RH

--- BitBoxSkeleton/src/main/java/unimelb/bitbox/ResponseHandler.java
+++ BitBoxSkeleton/src/main/java/unimelb/bitbox/ResponseHandler.java
@@ -128,10 +128,42 @@ public class ResponseHandler {
     }
 
     public void receivedFileDeleteRequest(Document d){
+    	String pathName = d.getString("pathName");
+    	Document desc = (Document)d.get("fileDescriptor");
+    	FileSystemManager.FileDescriptor fDesc =
+                fManager.new FileDescriptor(desc.getLong("lastModified"),desc.getString("md5"),desc.getLong("fileSize"));
+    	
+    	if(fManager.isSafePathName(pathName)) {
+    		try {
+				if(fManager.checkShortcut(pathName)){
+					//file has been found and try to delete
+					if(fManager.deleteFile(pathName,desc.getLong("lastModified"),desc.getString("md5"))) {
+						connection.sendCommand(JsonUtils.FILE_DELETE_RESPONSE(fDesc, pathName, "file deleted", true));
+					}else {
+						connection.sendCommand(JsonUtils.FILE_DELETE_RESPONSE(fDesc, pathName, "there was a problem deleting the file", false));
+					}
+					
+					
+				}else {
+					//there was a problem deleting the file
+					connection.sendCommand(JsonUtils.FILE_DELETE_RESPONSE(fDesc, pathName, "pathname does not exist", false));
+				}
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				log.warning(e.getMessage());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				log.warning(e.getMessage());
+			}
+    	}else {
+    		//unsafe pathname given
+    		connection.sendCommand(JsonUtils.FILE_DELETE_RESPONSE(fDesc, pathName, "unsafe pathname given", false));
+    		
+    	}
+    	
     }
 
-    public void receivedFileDeleteResponse(Document d){
-    }
+    public void receivedFileDeleteResponse(Document d){}
 
     public void receivedFileModifyRequest(Document d){
     }
@@ -139,11 +171,68 @@ public class ResponseHandler {
     public void receivedFileModifyResponse(Document d){
     }
 
-    public void receivedDirectoryCreateRequest(Document d){}
+    public void receivedDirectoryCreateRequest(Document d){
+    	String pathName = d.getString("pathName");
+   	
+    	if(fManager.isSafePathName(pathName)) {
+    		try {
+				if(!fManager.checkShortcut(pathName)){
+					//create directory
+					if(fManager.makeDirectory(pathName)) {
+						connection.sendCommand(JsonUtils.DIRECTORY_CREATE_RESPONSE( pathName, "directory created", true));
+					}else {
+						connection.sendCommand(JsonUtils.DIRECTORY_CREATE_RESPONSE( pathName, "there was a problem creating the directory", false));
+					
+					}				
+				}
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				log.warning(e.getMessage());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				log.warning(e.getMessage());
+			}
+    	}else {
+    		//unsafe pathname given
+    		connection.sendCommand(JsonUtils.DIRECTORY_CREATE_RESPONSE(pathName, "unsafe pathname given", false));
+    		
+    	}
+    	
+    }
 
     public void receivedDirectoryCreateResponse(Document d){}
 
-    public void receivedDirectoryDeleteRequest(Document d){}
+    public void receivedDirectoryDeleteRequest(Document d){
+    	String pathName = d.getString("pathName");
+       	
+    	if(fManager.isSafePathName(pathName)) {
+    		try {
+				if(fManager.checkShortcut(pathName)){
+					//directory has been found and then then try to delete
+					if(fManager.deleteDirectory(pathName)) {
+						connection.sendCommand(JsonUtils.DIRECTORY_CREATE_RESPONSE( pathName, "directory deleted", true));
+					}else {
+						connection.sendCommand(JsonUtils.DIRECTORY_CREATE_RESPONSE( pathName, "there was a problem creating the directory", false));
+					
+					}
+					
+				}else {
+					//pathname does not exist
+					connection.sendCommand(JsonUtils.DIRECTORY_CREATE_RESPONSE( pathName, "pathname does not exist", false));
+				}
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				log.warning(e.getMessage());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				log.warning(e.getMessage());
+			}
+    	}else {
+    		//unsafe pathname given
+    		connection.sendCommand(JsonUtils.DIRECTORY_CREATE_RESPONSE(pathName, "unsafe pathname given", false));
+    		
+    	}
+    }
 
     public void receivedDirectoryDeleteResponse(Document d){}
 
